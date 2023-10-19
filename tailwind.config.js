f435f2d3f4b93d8119945e918d4ff3f422a742f3
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-10-19 15:50:52 +0900

    init

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  important: true,
+  
+  content: [
+    "./src/**/*.{html}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
+
